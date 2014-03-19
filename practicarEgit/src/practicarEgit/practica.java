3c85d32a9e519012ccfa4e8d86383f74f5b39b80
MiguelAngel <MiguelAngel@Megui>
2014-03-19 10:45:40 +0100

    jjhb

--- practicarEgit/src/practicarEgit/practica.java
+++ practicarEgit/src/practicarEgit/practica.java
@@ -5,6 +5,13 @@ public class practica {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("tus muertos oidogfmeo");
+System.out.println("");
+for (int i = 0; i < 10; i++) {
+	int z = 1;
+	z++;
+	System.out.println(z);
+	
+}
 	}
 
 }
